06858a34881f39399f32d47e6709f572f062abd0
RomanDyukoff <Hlebahleb@gmail.com>
2021-04-03 23:07:50 +0300

    fix: bugs arrow slider, update hero

--- js/mine.js
+++ js/mine.js
@@ -1,60 +1,34 @@
 var appendNumber = 4;
-    var prependNumber = 1;
-    new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      centeredSlides: true,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button--next',
-        prevEl: '.swiper-button--prev',
-      },
-    });
-    document.querySelector('.prepend-2-slides').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.prependSlide([
-        '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>',
-        '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>'
-      ]);
-    });
-    document.querySelector('.prepend-slide').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.prependSlide('<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>');
-    });
-    document.querySelector('.append-slide').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.appendSlide('<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>');
-    });
-    document.querySelector('.append-2-slides').addEventListener('click', function (e) {
-      e.preventDefault();
-      swiper.appendSlide([
-        '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>',
-        '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>'
-      ]);
-    });
+var prependNumber = 1;
 
-    
+new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button--prev',
+    prevEl: '.swiper-button--next',
+  },
+  slideChange: (e) => {
+    console.log('KEk');
+  },
+})
 
+const sl = new Swiper('.swiper-container-2', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.reviews-slider__button--prev',
+  },
+  slideChange: (e) => {
+    console.log('KEk');
+  }
+});
 
-    var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 2,
-      direction: getDirection(),
-      navigation: {
-        prevEl: '.reviews-slider__button--prev',
-      },
-      on: {
-        resize: function () {
-          swiper.changeDirection(getDirection());
-        }
-      }
-    });
+const button = document.querySelector('')
 
-    function getDirection() {
-      var windowWidth = window.innerWidth;
-      var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+// button.addEventListener('click', () => {
+//   sl.slideNext()
+// })
 
-      return direction;
-    }
+// slick slider
